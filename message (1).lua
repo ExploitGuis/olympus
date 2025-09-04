f0f5c1883bef8c05722204495754652cd4a9dcb9
ExploitGuis <127593599+ExploitGuis@users.noreply.github.com>
2025-09-04 11:55:21 -0500

    Update message (1).lua

--- message (1).lua
+++ message (1).lua
@@ -2572,7 +2572,7 @@ GameTab:CreateToggle({
 })
 
 GameTab:CreateToggle({
-    Name = "AutoKill Server (Hera)",
+    Name = "AutoKill Server (BUGGED)",
     CurrentValue = false,
     Flag = "Toggle1",
     Callback = function(Value)
@@ -2728,10 +2728,11 @@ GameTab:CreateToggle({
 EvntTab:CreateSection("Info")
 EvntTab:CreateParagraph({Title = "Olympus Script Developers🛠️", Content = "Kermisha & MotherOfGrass & Moon"})
 EvntTab:CreateParagraph({Title = "Support Discord DMS💬", Content = "kermishaurfavx & grassmother & themoonandsunreuinite"})
-EvntTab:CreateParagraph({Title = "Script Updates✨", Content = "MINI Update: Telekenisis Glitch+Mass Earthquake+Auto Kill"})
+EvntTab:CreateParagraph({Title = "Script Updates✨", Content = "MINI Update: Telekenisis Glitch+Mass Earthquake"})
 EvntTab:CreateParagraph({Title = "Ban Risk⛔", Content = "MEDIUM"})
 EvntTab:CreateParagraph({Title = "Exploit Patches🧪", Content = "0 - yay"})
 EvntTab:CreateParagraph({Title = "Note From Hub Developers📝", Content = "If you don't wanna get banned from olympus don't use stuff that people can record and report, everything else is safe <3"})
+
 
 
 
